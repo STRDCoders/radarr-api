cf5ed6e9febfe051d33426f53e49e26d4fee1d0f
Tomer Blecher <tomer.blecher.brolix@gmail.com>
2022-06-05 15:16:35 +0300

    Add some extra DTO props

--- src/model/RadarrQueueRecord.ts
+++ src/model/RadarrQueueRecord.ts
@@ -1,8 +1,28 @@
-import { MediaDownloadQueueItemClientStatus, MediaDownloadQueueItemTrackedStatus } from "./RadarrQueueStatus";
-
 export interface RadarrQueueRecord {
+  id: number;
+  movieId?: number;
   title: string;
   timeleft: string;
+  sizeleft: number;
+  size: number;
   status: MediaDownloadQueueItemClientStatus;
   trackedDownloadStatus: MediaDownloadQueueItemTrackedStatus;
+  languages?: Language[];
+}
+
+export enum MediaDownloadQueueItemClientStatus { // status for radarr
+  paused = "paused",
+  downloading = "downloading",
+  completed = "completed",
+}
+
+export enum MediaDownloadQueueItemTrackedStatus { // trackedDownloadStatus for radarr
+  warning = "warning",
+  ok = "ok",
+  error = "error",
+}
+
+export interface Language {
+  id: number;
+  name?: string;
 }
